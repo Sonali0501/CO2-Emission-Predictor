c6e9a6590630fc850a01e4150e435499c6c485f6
VKAL1091 <kalshettividyashri1901@gmail.com>
2021-06-16 12:34:01 +0530

    table displayed

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import csv
+import pandas as pd
+
+
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -11,21 +15,31 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['GET','POST'])
 def predict():
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    if request.method =='GET':
+        return render_template('index.html')
+    elif request.method=='POST':
+        int_features = [float(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+        output = round(prediction[0], 2)
 
-    return render_template('index.html',eng_size=int_features[0], cylinders=int_features[1], fuel=int_features[2], emission=output, scroll="prediction")
+        return render_template('index.html',eng_size=int_features[0], cylinders=int_features[1], fuel=int_features[2], emission=output, scroll="prediction")
 
 @app.route('/suggest',methods=['POST'])
 def suggest():
     filters = [x for x in request.form.values()]
+    
+    data = pd.read_csv("FuelConsumption.csv")
 
-    return render_template('index.html', scroll="suggestion",fuel_type=filters[0], vehicle_class=filters[1], make=filters[2] ,results=True)
+    
+    filt=data[(data['FUELTYPE']==filters[0])|(data['VEHICLE CLASS']==filters[1])|(data['MAKE']==filters[2])]
+    
+    dataset=filt.loc[:,['FUELTYPE','VEHICLE CLASS','MAKE','MODEL','CO2EMISSIONS']]
+    
+    return render_template('index.html', scroll="suggestion",fuel_type=filters[0], vehicle_class=filters[1], make=filters[2] ,results=True, data=dataset.head(8).to_html())
 
 
 if __name__ == "__main__":
--- mlmodel.py
+++ mlmodel.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
 
-dataset = pd.read_csv('FuelConsumptionCo2.csv')
+dataset = pd.read_csv('FuelConsumption.csv')
 X = dataset.iloc[:, 4:6].values
 X = np.concatenate((X,dataset.iloc[:, 10:11].values),axis=1) 
 Y = dataset.iloc[:, -1].values
