130abf4f75d0e763d7f6ba2a06ff4af591ac0bbe
Sonali0501 <nogjasonali8@gmail.com>
2021-06-16 13:35:46 +0530

    Data filters for table

--- app.py
+++ app.py
@@ -31,15 +31,34 @@ def predict():
 @app.route('/suggest',methods=['POST'])
 def suggest():
     filters = [x for x in request.form.values()]
+    print(filters)
     
     data = pd.read_csv("FuelConsumption.csv")
 
-    
-    filt=data[(data['FUELTYPE']==filters[0])|(data['VEHICLE CLASS']==filters[1])|(data['MAKE']==filters[2])]
-    
+    if (filters[0]=='' and filters[1]=='' and filters[2]==''):
+        filt=data.head(8)
+    elif filters[0]=='' and filters[1]=='':
+        filt=data[(data['MAKE']==filters[2])]
+    elif filters[0]=='' and filters[2]=='':
+        filt=data[(data['VEHICLE CLASS']==filters[1])]
+    elif filters[1]=='' and filters[2]=='':
+        filt=data[(data['FUELTYPE']==filters[0])]
+    elif filters[2]=='':
+        filt=data[(data['FUELTYPE']==filters[0])&(data['VEHICLE CLASS']==filters[1])]
+    elif filters[1]=='':
+        filt=data[(data['FUELTYPE']==filters[0])&(data['MAKE']==filters[2])]
+    elif filters[0]=='':
+        filt=data[(data['VEHICLE CLASS']==filters[1])&(data['MAKE']==filters[2])]
+    elif (filters[0]!='' and filters[1]!='' and filters[2]!=''):
+        filt=data[(data['FUELTYPE']==filters[0])&(data['VEHICLE CLASS']==filters[1])&(data['MAKE']==filters[2])]
+        
+
     dataset=filt.loc[:,['FUELTYPE','VEHICLE CLASS','MAKE','MODEL','CO2EMISSIONS']]
+
+    if len(dataset)==0:
+        return render_template('index.html', scroll="suggestion",fuel_type=filters[0], vehicle_class=filters[1], make=filters[2] ,results=True, not_found=True)
     
-    return render_template('index.html', scroll="suggestion",fuel_type=filters[0], vehicle_class=filters[1], make=filters[2] ,results=True, data=dataset.head(8).to_html(classes = "list"))
+    return render_template('index.html', scroll="suggestion",fuel_type=filters[0], vehicle_class=filters[1], make=filters[2] ,results=True, data=dataset.head(8).to_html(index=False,classes = "list"))
 
 
 if __name__ == "__main__":
